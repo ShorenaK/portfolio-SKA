5bf6ec1c3d09595ce56ca452dda9e67660632024
ShorenaK <shorenaanzhilov@gmail.com>
2022-10-25 15:18:20 -0400

    fonts installed

---
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {TOTAL_SCREENS, GET_SCREEN_INDEX} from 
+'../../../utilities/commonUtils'
+
+import ScrollService from '../../../utilities/ScrollService'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import '.Header.css'
+
+export default function Header() {
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
